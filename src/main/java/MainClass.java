d2a30030343c71f84e91d50a4d70f6fa9606eb31
ayla <ayladebora@gmail.com>
2026-09-21 10:00:39 -0300

    Métodos revisão

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite nomes separados por ;");
+        String [] nomes = leitor.nextLine().split(";");
+        System.out.println("Nomes lidos:");
+        int k=0;
+        do {
+            System.out.println(nomes[k]);
+            k++;
+        } while(k<nomes.length);
+
+        leitor.close();
+    }
+}
---
+++ src/main/java/TestaMetodo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class TestaMetodo {
+
+    public static void main(String [] args){
+        System.out.println(args[0]);
+        TestaMetodo.digaOlaDezVezes();
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite um número inteiro");
+        int numero = Integer.parseInt(leitor.nextLine());
+        if (ehMultiploDe3(numero)){
+            System.out.println("O número "+numero+" é múltiplo de 3");
+        } else {
+            System.out.println("O número "+numero+" não é múltiplo de 3");
+        }
+        leitor.close();
+    }
+
+    public static boolean ehMultiploDe3(int numeroAPesquisar){
+        if (numeroAPesquisar%3==0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void digaOlaDezVezes(){
+        int k=10;
+        do {
+            System.out.println("Olá "+k);
+            k--;
+        } while(k>0);
+    }
+
+}
